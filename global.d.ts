4c11f572fed63f72ed3f903d677f6ccd71d82734
antonis.paterakis <a.paterakis@ysoft.gr>
2023-01-06 19:54:40 +0200

    add global variables

---
+++ global.d.ts
@@ -0,0 +1,12 @@
+import { NodeCache } from 'node-cache';
+
+import AppCache from './src/services/appCache.service';
+import SportsCache from './src/services/sportsCache.service';
+import EventsCache from './src/services/eventsCache.service';
+
+declare global {
+    var cache: NodeCache;
+    var appCache: AppCache;
+    var sportsCache: SportsCache;
+    var eventsCache: EventsCache;
+}
