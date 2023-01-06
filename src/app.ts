497c3dbef152ee4c2fdc4255233aa60453b41e54
antonis.paterakis <a.paterakis@ysoft.gr>
2023-01-07 00:36:31 +0200

    init i18n

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,19 @@
 import AppController from "./controllers/app.controller";
 import NodeCache from "node-cache";
-
+import { I18n } from "i18n";
+import path from "path";
 import AppCache from "./services/appCache.service";
 import SportsCache from "./services/sportsCache.service";
 import EventsCache from "./services/eventsCache.service";
  
+const i18n = new I18n({
+    locales: ['en', 'de', 'zh'],
+    defaultLocale: 'en',
+    directory: path.join('./', 'locales')
+});
+
+// @ts-ignore
+global.i18n = i18n;
 global.cache = new NodeCache();
 global.appCache = new AppCache();
 global.sportsCache = new SportsCache();
